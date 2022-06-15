42abb4ceaf05f64ad3a5d4cf10638c995dca51fb
Jose <insesions@hotmail.com>
2022-06-15 21:43:46 +0200

    Cuarta parte Ejercicio Tema 4

--- src/Main.java
+++ src/Main.java
@@ -31,7 +31,7 @@ public class Main {
     }
 
     System.out.println("-------------");
-     //Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+    //Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
 
     int numeroDoWhile = 3;
 
@@ -39,6 +39,24 @@ public class Main {
         System.out.println("El número es: " + numeroDoWhile);
         numeroDoWhile++;
     } while (numeroDoWhile<3);
+
+
+
+    System.out.println("-------------");
+    /*Para el bucle For, crea una variable numeroFor,
+    esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3,
+    se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
+     */
+
+
+
+    for (int numeroFor =0; numeroFor<=3; numeroFor++ ){
+
+        System.out.println("El número es: " + numeroFor);
+
+    }
+
+
 
 
 
