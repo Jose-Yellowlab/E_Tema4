6861c6171dc54dae13de9f1d9d240fe9f1eddb33
Jose <insesions@hotmail.com>
2022-06-15 21:23:35 +0200

    Segunda parte Ejercicio Tema 4

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,23 @@ public class Main {
     } else {
         System.out.println("El número es igual a 0");
     }
+
+    /*Crea un bucle While, este bucle tendrá que tener como condición que la
+      variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+      Incrementar el valor de la variable en uno cada vez que se ejecute.
+      Mostrarlo por pantalla cada vez que se ejecute.
+    */
+
+    int numeroWhile = -10;
+
+    while (numeroWhile<3) {
+        System.out.println("El número es: " + numeroWhile);
+        numeroWhile++;
+
+    }
+
+
+
+
     }
 }
