38ba0ea36b499b32d288e03cc53bd536fb4dc0c1
Jose <insesions@hotmail.com>
2022-06-16 10:22:00 +0200

    Ejercicio Terminado Tema 4

--- src/Main.java
+++ src/Main.java
@@ -33,11 +33,12 @@ public class Main {
     System.out.println("-------------");
     //Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
 
-    int numeroDoWhile = 3;
+    int numeroDoWhile = 2;
 
     do {
-        System.out.println("El número es: " + numeroDoWhile);
         numeroDoWhile++;
+        System.out.println("El número es: " + numeroDoWhile);
+
     } while (numeroDoWhile<3);
 
 
@@ -56,11 +57,32 @@ public class Main {
 
     }
 
+    System.out.println("-------------");
+    /*Por último, para el Switch, deberás crear la variable estacion,
+    y distintos case para las cuatro estaciones del año.
+    Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola
+    informando de la estación en la que está.
+    También habrá que poner un default para cuando el valor de la variable no sea una estación.
+     */
 
+     String estacion = "Primavera";
 
+     switch (estacion){
+         case "Primavera":
+             System.out.println("La estación es: " + estacion);
+             break;
+         case "Verano":
+             System.out.println("La estación es: " + estacion);
+             break;
+         case "Otoño":
+             System.out.println("La estación es: " + estacion);
+             break;
+         case "Invierno":
+             System.out.println("La estación es: " + estacion);
+             break;
+         default: System.out.println("Esto no es una estación");
 
-
-
+     }
 
     }
 }
